3cf67f864e939668aae1416495061e2438ce93f0
denizdagli <denizdaglice@gmail.com>
2025-02-12 12:37:35 +0300

    Updated bot.py: Improved help command and added command descriptions

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from commands import handle_add_task, handle_delete_task, handle_show_tasks, handle_complete_task
 
 load_dotenv()
-
 token = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
@@ -17,23 +16,22 @@ async def on_ready():
     print(f"Bot {bot.user} olarak giriş yaptı!")
     await bot.change_presence(activity=discord.Game(name="!add, !delete, !show, !complete"))
 
-
-@bot.command()
+@bot.command(help="Yeni bir görev ekler.")
 async def add(ctx, *args):
     response = handle_add_task(args)
     await ctx.send(response)
 
-@bot.command()
+@bot.command(help="Belirtilen ID'ye sahip görevi siler.")
 async def delete(ctx, *args):
     response = handle_delete_task(args)
     await ctx.send(response)
 
-@bot.command()
+@bot.command(help="Tüm görevleri listeler.")
 async def show(ctx):
     response = handle_show_tasks()
     await ctx.send(response)
 
-@bot.command()
+@bot.command(help="Belirtilen ID'ye sahip görevi tamamlandı olarak işaretler.")
 async def complete(ctx, *args):
     response = handle_complete_task(args)
     await ctx.send(response)
